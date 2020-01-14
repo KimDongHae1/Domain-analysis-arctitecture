c551c9ab86ff57d6739d300a3d05651f4df959ae
KimDongHae1 <55433565+KimDongHae1@users.noreply.github.com>
2020-01-15 01:24:10 +0900

    Add files via upload

---
+++ HI/HI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HI
+//
+//  Created by MAC on 2019/12/08.
+//  Copyright © 2019 MAC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
